db10bb64fa5e31f354c362e05d1662efac98782f
Rathina Boopathi Sundarraj <rathinaboopathi.srb@gmail.com>
2023-10-07 22:10:47 +0530

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start">
-      <div className="relative flex h-[50vh] max-h-screen place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial
+      <div className="relative flex h-[50vh] lg:h-full w-full max-h-screen place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial
        before:from-white before:to-transparent before:blur-2xl before:content-[''] 
        after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 
        after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10
